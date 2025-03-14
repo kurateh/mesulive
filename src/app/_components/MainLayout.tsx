3712775cfa22cfcb23a93901661f278396cc9c51
-g <me@kurateh.com>
2025-03-14 21:52:41 +0900

    Deactivate Ad temporary

--- src/app/_components/MainLayout.tsx
+++ src/app/_components/MainLayout.tsx
@@ -3,7 +3,6 @@
 import { useRef, type ReactNode } from "react";
 
 import { cx } from "~/shared/style";
-import { Adsense } from "~/shared/ui";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -27,10 +26,10 @@ export const MainLayout = ({ children, disableFooter }: Props) => {
           className="mt-[-65px] h-screen flex-1 overflow-y-auto bg-default-50 pt-[65px]"
         >
           <div className={cx("p-4")}>
-            <Adsense
+            {/* <Adsense
               adSlot="6642780063"
               className="mx-auto mb-4 block h-[50px] w-[300px] md:h-[90px] md:w-[728px]"
-            />
+            /> */}
             {children}
             {/* <div className="flex justify-evenly gap-4">
               <Adsense
@@ -43,11 +42,11 @@ export const MainLayout = ({ children, disableFooter }: Props) => {
                 className="sticky top-10 hidden h-[600px] w-[160px] shrink-0 2xl:inline-block"
               />
             </div> */}
-            <Adsense
+            {/* <Adsense
               adSlot="6642780063"
               className="mx-auto mt-16 block w-[300px] md:w-[728px]"
               adFormat="auto"
-            />
+            /> */}
           </div>
           {!disableFooter && <Footer />}
         </div>
