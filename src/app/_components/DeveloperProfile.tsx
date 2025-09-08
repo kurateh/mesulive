08bf8ddae9a00cd5ef06a6041bafd4b4bc86827f
-g <me@kurateh.com>
2025-09-08 19:28:26 +0900

    Replace profile image src

--- src/app/_components/DeveloperProfile.tsx
+++ src/app/_components/DeveloperProfile.tsx
@@ -1,53 +1,11 @@
 import { Image, Link } from "@heroui/react";
 import { Equal } from "lucide-react";
 import NextImage from "next/image";
-import { z } from "zod";
 
 import { Reboot } from "~/shared/assets/images";
 import { cx } from "~/shared/style";
 
-const idResponseSchema = z.object({
-  ocid: z.string(),
-});
-
-const characterResponseSchema = z.object({
-  // eslint-disable-next-line camelcase
-  character_image: z.string(),
-});
-
 export const DeveloperProfile = async () => {
-  const imageSrc = await fetch(
-    `https://open.api.nexon.com/maplestory/v1/id?${new URLSearchParams({
-      // eslint-disable-next-line camelcase
-      character_name: "쿠라테",
-    })}`,
-    {
-      headers: { "x-nxopen-api-key": process.env.NEXON_OPEN_API_KEY! },
-      cache: "force-cache",
-      next: { revalidate: 86400 },
-    },
-  )
-    .then((res) => res.json())
-    .then(idResponseSchema.parse)
-    .then(({ ocid }) =>
-      fetch(
-        `https://open.api.nexon.com/maplestory/v1/character/basic?${new URLSearchParams(
-          {
-            ocid,
-          },
-        )}`,
-        {
-          headers: { "x-nxopen-api-key": process.env.NEXON_OPEN_API_KEY! },
-          cache: "force-cache",
-          next: { revalidate: 86400 },
-        },
-      ),
-    )
-    .then((res) => res.json())
-    .then(characterResponseSchema.parse)
-    // eslint-disable-next-line camelcase
-    .then(({ character_image }) => character_image);
-
   return (
     <div>
       <p className="text-center text-sm font-bold text-default-500">제작자</p>
@@ -60,9 +18,12 @@ export const DeveloperProfile = async () => {
           color="foreground"
         >
           <Image
-            src={imageSrc}
+            src="https://avatar.maplestory.nexon.com/Character/180/GDEOHJKMEGLKDLKHKOJOPKHHDPFLPGFHOBLLHPGOMEJBMOBPPMIKOJFPPNBPNLNMKIEKEKMFDKPCFFIMLMAOJEGLGCAKMIPKKJCBGFBEPAEONEIADBONEJGKPPPCKHEPIMCLMJAPNMAIAOGEMDKFDKLEHGBOONMNEBDBHDNJLGAFAGBCBIFENNBPILMEMADCGCMKAFOADABEGNIEKGANLEMHAPMPHJNGMDCCCLAHGJLFAPLMLAFGCBCDADPLNNPM.png"
             alt="제작자"
-            classNames={{ img: cx("pixelated") }}
+            classNames={{
+              img: cx("pixelated mt-[-10px] object-none"),
+              wrapper: cx("h-24 w-24"),
+            }}
           />
           <div className="flex items-center">
             <p>쿠라테</p>
