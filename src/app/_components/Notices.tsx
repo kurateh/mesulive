5361818f1c125e29c807d79e17f872b04a379d4b
-g <me@kurateh.com>
2026-04-19 17:00:47 +0900

    Remove error fix notice

--- src/app/_components/Notices.tsx
+++ src/app/_components/Notices.tsx
@@ -41,31 +41,6 @@ const noticeContents: (Optional<
     },
     icon: <CirclePlus />,
   },
-  {
-    variant: "solid",
-    color: "danger",
-    description: (
-      <>
-        <Link
-          href="/sim/starforce"
-          className="text-sm font-bold text-white"
-          underline="always"
-          showAnchorIcon
-        >
-          스타포스 시뮬레이터
-        </Link>
-        3/22 13:18 24성 이상을 목표로 시뮬레이션을 진행할 때 흔적 복구 기능이
-        정상적으로 작동하지 않는 문제가 있었습니다. 현재는 수정된 상태이며,
-        불편을 드려 죄송합니다.
-      </>
-    ),
-    date: new Date("2026-03-21"),
-    classNames: {
-      base: cx("bg-danger-500"),
-      alertIcon: cx("fill-white text-danger-500"),
-    },
-    icon: <CirclePlus />,
-  },
 ];
 
 export const revalidate = 86400;
